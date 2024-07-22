2c3bafb6ffc2de21ea143299d4d317794682a9cb
dogmania <hj296914@gmail.com>
2024-07-22 23:50:48 +0900

    블로그 글 생성 API 구현

---
+++ Blog/src/main/java/org/example/springbootdeveloper/controller/BlogApiController.java
@@ -0,0 +1,26 @@
+package org.example.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.domain.Article;
+import org.example.springbootdeveloper.dto.AddArticleRequest;
+import org.example.springbootdeveloper.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController // Http Response Body에 객체 데이터를 Json 형식으로 반환하는 컨트롤러
+public class BlogApiController {
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) { // @RequestBody로 요청 본문 값 매핑
+        Article savedArticle = blogService.save(request);
+
+        // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+}
---
+++ Blog/src/main/java/org/example/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,17 @@
+package org.example.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.domain.Article;
+import org.example.springbootdeveloper.dto.AddArticleRequest;
+import org.example.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor    // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service    // 빈으로 등록
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}

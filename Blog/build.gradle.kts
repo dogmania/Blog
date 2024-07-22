d393b9124ce077e13f7d6d3df36684c0e329f968
dogmania <hj296914@gmail.com>
2024-07-22 23:49:47 +0900

    스프링 부트 의존성 추가

--- Blog/build.gradle.kts
+++ Blog/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.0.2"
+    id("io.spring.dependency-management") version "1.1.0"
 }
 
 group = "org.example"
@@ -10,8 +12,18 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // 스프링 데이터 JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // 인메모리 데이터베이스
+    runtimeOnly("com.h2database:h2")
+
+    // 롬복
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.test {

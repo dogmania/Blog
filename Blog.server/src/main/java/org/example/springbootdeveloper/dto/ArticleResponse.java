47fe81a74c2ad1bf9eeb1652ca50f63b94d34840
dogmania <hj296914@gmail.com>
2024-07-24 16:32:26 +0900

    반환값에 id, createdAt, updateAt 추가

--- Blog.server/src/main/java/org/example/springbootdeveloper/dto/ArticleResponse.java
+++ Blog.server/src/main/java/org/example/springbootdeveloper/dto/ArticleResponse.java
@@ -1,16 +1,27 @@
 package org.example.springbootdeveloper.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import org.example.springbootdeveloper.domain.Article;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class ArticleResponse {
 
+    private final Long id;
     private final String title;
     private final String content;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private final LocalDateTime createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private final LocalDateTime updatedAt;
 
     public ArticleResponse(Article article) {
+        this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+        this.updatedAt = article.getUpdatedAt();
     }
 }

fbe7adb6ae72c2258f1db149fcf0f1d7603dc668
dogmania <hj296914@gmail.com>
2024-07-31 14:24:39 +0900

    로그인 요청에 대해 이메일과 패스워드를 비교하는 로직 구현

--- Blog.server/src/main/java/org/example/springbootdeveloper/service/UserService.java
+++ Blog.server/src/main/java/org/example/springbootdeveloper/service/UserService.java
@@ -1,17 +1,26 @@
 package org.example.springbootdeveloper.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.config.jwt.TokenProvider;
 import org.example.springbootdeveloper.domain.User;
 import org.example.springbootdeveloper.dto.AddUserRequest;
+import org.example.springbootdeveloper.dto.CreateAccessTokenResponse;
+import org.example.springbootdeveloper.dto.LoginRequest;
 import org.example.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final TokenProvider tokenProvider;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Long save(AddUserRequest dto) {
@@ -19,6 +28,19 @@ public class UserService {
                 .email(dto.getEmail())
                 .password(bCryptPasswordEncoder.encode(dto.getPassword())) // 패스워드 암호화
                 .build()).getId();
+    }
+
+    public CreateAccessTokenResponse findByEmailPassword(LoginRequest dto) {
+        Optional<User> user = userRepository.findByEmail(dto.getEmail());
+        user.orElseThrow(() -> new UsernameNotFoundException("User not fount"));
+
+        if (!user.get().getPassword().equals(dto.getPassword())) {
+            throw new BadCredentialsException("Password is incorrect");
+        }
+
+        return new CreateAccessTokenResponse(
+                tokenProvider.generateToken(user.get(), Duration.ofHours(2))
+        );
     }
 
     public User findById(Long userId) {

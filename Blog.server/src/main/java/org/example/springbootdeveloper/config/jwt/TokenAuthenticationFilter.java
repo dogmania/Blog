95e3e15decce65563566293441e7a8ca7568277a
dogmania <hj296914@gmail.com>
2024-08-03 00:41:04 +0900

    TokenAuthenticationFilter 빈으로 등록

--- Blog.server/src/main/java/org/example/springbootdeveloper/config/jwt/TokenAuthenticationFilter.java
+++ Blog.server/src/main/java/org/example/springbootdeveloper/config/jwt/TokenAuthenticationFilter.java
@@ -5,17 +5,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
     private final TokenProvider tokenProvider;
     private final static String HEADER_AUTHORIZATION = "Authorization";
     private final static String TOKEN_PREFIX = "Bearer ";
+
+    private static final Logger log = LoggerFactory.getLogger(TokenAuthenticationFilter.class);
 
     @Override
     protected void doFilterInternal(
@@ -27,6 +33,9 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
         String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
         // 가져온 값에서 접두사 제거
         String token = getAccessToken(authorizationHeader);
+
+        log.info("Authorization Header: " + authorizationHeader);
+        log.info("Extracted Token: " + token);
 
         // 가져온 토큰이 유효한지 확인하고, 유효한 때는 인증 정보 설정
         if (token != null && tokenProvider.validToken(token)) {

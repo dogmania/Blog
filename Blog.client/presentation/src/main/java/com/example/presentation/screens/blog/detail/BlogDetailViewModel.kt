c6d7a1efc199abacf8e33c141cdf6adf71873069
dogmania <hj296914@gmail.com>
2024-08-03 01:18:40 +0900

    블로그 상세 조회 페이지, ViewModel, PageState 구현

---
+++ Blog.client/presentation/src/main/java/com/example/presentation/screens/blog/detail/BlogDetailViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.presentation.screens.blog.detail
+
+import androidx.lifecycle.viewModelScope
+import com.example.domain.model.response.ArticleResponseVo
+import com.example.domain.usecase.blog.GetArticleUseCase
+import com.example.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BlogDetailViewModel @Inject constructor(
+    private val getArticleUseCase: GetArticleUseCase
+) : BaseViewModel<BlogDetailPageState>(BlogDetailPageState()) {
+
+    fun getArticle(id: Long) {
+        viewModelScope.launch {
+            getArticleUseCase.invoke(id).collect {
+                resultResponse(it, ::getArticleSuccessHandler)
+            }
+        }
+    }
+
+    private fun getArticleSuccessHandler(article: ArticleResponseVo) {
+        updateState(
+            uiState.value.copy(
+                title = article.title,
+                content = article.content
+            )
+        )
+    }
+}
